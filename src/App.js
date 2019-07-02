0393403a0dca88a69d77609e1bc47867c33a5505
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2019-07-02 15:12:01 -0400

    Added styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
@@ -12,10 +13,12 @@ function App() {
   return (
     <React.Fragment>
       <Navbar />
-      <ProductList />
-      <Details />
-      <Cart />
-      <Default />
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Default} />
+      </Switch>
     </React.Fragment>
   );
 }
--- src/components/Productlist.js
+++ src/components/Productlist.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 import Product from "./Product";
+import Title from "./Title";
 export default class ProductList extends Component {
+  state = {
+    products: []
+  };
   render() {
+    console.log(this.state.products);
     return (
       <div>
-        <Product />
+        <React.Fragment>
+          <div className="py-5">
+            <div className="container">
+              <Title name="our" title="products" />
+              <div className="row" />
+            </div>
+          </div>
+        </React.Fragment>
       </div>
     );
   }
