9596d79242c3347672ce9712e94efe8250715bc6
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2019-07-02 18:28:22 -0400

    Add context API to the Details component and finish addToCart method

--- src/context.js
+++ src/context.js
@@ -8,26 +8,49 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
   state = {
     products: [],
-    detailProduct: detailProduct
+    detailProduct: detailProduct,
+    cart: [],
+    modalOpen: true
   };
   componentDidMount() {
-      this.setProducts();
+    this.setProducts();
   }
   setProducts = () => {
-      let tempProducts = [];
-      storeProducts.forEach(item => {
-        const singleItem = {...item}; //copying the values
-        tempProducts = [... tempProducts, singleItem];
-      })
-      this.setState((0  =>  {
-          return {products: tempProducts}
-      }))
-  }
-  handleDetail = () => {
-    console.log("hello from detail");
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item }; //copying the values
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return { products: tempProducts };
+    });
   };
-  addToCart = () => {
-    console.log("hello from add to cart");
+  handleDetail = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
+  };
+  getItem = id => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
+  };
+  addToCart = id => {
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+    this.setState(
+      () => {
+        return { products: tempProducts, cart: [...this.state.cart, product] };
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
   };
 
   render() {
